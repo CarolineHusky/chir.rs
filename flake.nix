c7a77cad9111be9fcdefb677c592a076e6d92482
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-01-14 10:53:16 +0100

    ci: Update to rust 1.66.1

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       };
       rustPkgs = pkgs.rustBuilder.makePackageSet {
         packageFun = import ./Cargo.nix;
-        rustChannel = "1.65.0";
+        rustChannel = "1.66.1";
         packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
       };
     in rec {
